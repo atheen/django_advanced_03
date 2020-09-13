544ab46f8d565248edefe1359cf2270ac4d6d0b2
atheen <aatheen.ds@gmail.com>
2020-09-13 10:38:53 +0300

    completed some adjustments

--- stores/admin.py
+++ stores/admin.py
@@ -3,7 +3,8 @@ from .models import Store
 
 # Register your models here.
 class StoreAdmin(admin.ModelAdmin):
-    list_display = ['name','id','description']
+    list_display = ['id','name','description']
+    list_display_links = ['name']
     search_fields = ['name','description']
     list_filter = ['added']
     list_editable = ['description']
